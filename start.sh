65da7901070f4c282864ebf120bcb157a28b696f
1Pampu <piampianimartin@gmail.com>
2024-07-01 05:32:41 -0300

    copy from bind

--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Verifica si SSL_PATH está definido y no está vacío
-if [ -n "$SSL_PATH" ]; then
-    echo "SSL_PATH está definido. Copiando archivos SSL..."
-    mkdir -p /app/certs
-    cp ${SSL_PATH}/fullchain.pem /app/certs/
-    cp ${SSL_PATH}/privkey.pem /app/certs/
-else
-    echo "SSL_PATH no está definido. No se copiarán los archivos SSL."
-fi
+# Crear el directorio certs si no existe
+mkdir -p certs
+
+# Copiar los archivos desde el volumen montado a /app/certs
+cp bind/fullchain.pem certs/
+cp bind/privkey.pem certs/
 
 # Corre Gunicorn para servir la aplicación Django
 gunicorn --certfile certs/fullchain.pem --keyfile certs/privkey.pem --bind 0.0.0.0:8888 config.wsgi:application
