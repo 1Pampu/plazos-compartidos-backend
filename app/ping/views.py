e4bd16ef86bd50e5629d4705ccb84aa2054653c0
1Pampu <piampianimartin@gmail.com>
2024-07-12 19:02:41 -0300

    add time & date to ping

--- app/ping/views.py
+++ app/ping/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.utils import timezone
 import random
 
 @api_view(['GET'])
 def RandomNumberView(request):
     random_number = random.randint(1, 100)
-    return Response({'random_number': random_number})
+    return Response({'random_number': random_number, 'date': timezone.now().date(), 'time': timezone.now().time()})
